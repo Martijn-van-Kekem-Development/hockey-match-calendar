51a8d23a694c30f06c1d633862079ca5889840c9
Martijn van Kekem <martijn@vankekem.com>
2024-08-09 03:29:16 +0200

    Fix test cases broken

--- tests/Utils/Countries.test.ts
+++ tests/Utils/Countries.test.ts
@@ -2,23 +2,13 @@ import {describe, expect, test} from "vitest";
 import {Countries} from "../../src/Utils/Countries.js";
 
 describe("Countries tests", () => {
-    test("Get abbreviation - Netherlands", () => {
-        const output = Countries.getAbbr("Netherlands");
-        expect(output).toBe("NED");
+    test("Get by ISO - Netherlands", () => {
+        const output = Countries.getCountryByISO("NLD");
+        expect(output.full).toBe("Netherlands");
     });
 
-    test("Get abbreviation - Germany", () => {
-        const output = Countries.getAbbr("Germany");
-        expect(output).toBe("GER");
-    });
-
-    test("ISO to IOC - Netherlands", () => {
-        const output = Countries.ISOToIOC("NED");
-        expect(output).toBe("NED");
-    });
-
-    test("ISO to IOC - Germany", () => {
-        const output = Countries.ISOToIOC("DEU");
-        expect(output).toBe("GER");
+    test("Get by IOC - Netherlands", () => {
+        const output = Countries.getCountryByIOC("NED");
+        expect(output.full).toBe("Netherlands");
     });
 })
