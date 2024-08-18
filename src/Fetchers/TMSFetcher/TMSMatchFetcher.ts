a00ba01bc13452017b59c813c2bf970a78b9939b
philipohagan <phil.ohagan@gmail.com>
2024-08-18 22:08:08 +0100

    Rework Match Fetecher Regex to fix tests

--- src/Fetchers/TMSFetcher/TMSMatchFetcher.ts
+++ src/Fetchers/TMSFetcher/TMSMatchFetcher.ts
@@ -98,7 +98,7 @@ export class TMSMatchFetcher {
         const result = title
             .normalize("NFD")
             .replace(/[\u0300-\u036f]/g, "")
-            .match(/(?:([A-Za-z0-9/& -]*) v )?([A-Za-z0-9/& -]*)(?: \((.+)\))?$/);
+            .match(/^([A-Za-z0-9/& -]+) v ([A-Za-z0-9/& -]+)(?: \((.+)\))?$/);
 
         if (!result) throw new Error("Couldn't extract data from match title: " + title);
 
