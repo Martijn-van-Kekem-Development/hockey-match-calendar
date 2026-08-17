379c8324473dca7e7092b275c0ffeab76e32a434
Martijn van Kekem <martijn@vankekem.com>
2026-08-17 13:15:00 +0200

    fix: broken description URL after changing match IDs (#218)
    * fix: broken description URL after changing match IDs
    
    * fix: lint

--- src/Fetchers/AltiusFetcher/AltiusMatchFetcher.ts
+++ src/Fetchers/AltiusFetcher/AltiusMatchFetcher.ts
@@ -107,7 +107,7 @@ export class AltiusMatchFetcher {
             "error", "Skipping match, failed to get ID", {
                 "competition": `${competition.getID()}`,
             });
-        object.setID(id, competition);
+        object.setID(id);
 
         // Add match index
         const indexEl = row.querySelector("td:nth-child(1)");
